b65378f33086663a4b8e4582defb2a25b095c99e
Ryan Balla <ryan@ryanballa.com>
2021-03-13 17:42:45 -0600

    Trying serverless

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
-import { App } from '@tinyhttp/app'
-import sanity from './lib/sanity.js';
-import { cors } from '@tinyhttp/cors'
-import { logger } from '@tinyhttp/logger'
-import bodyParser from 'body-parser'
+// import { App } from '@tinyhttp/app'
+// import sanity from './lib/sanity.js';
+// import { cors } from '@tinyhttp/cors'
+// import { logger } from '@tinyhttp/logger'
+// import bodyParser from 'body-parser'
 
-const app = new App({
-    onError: (err, req, res) => {
-        res.status(500).send({
-            message: err.message,
-        })
-    },
-})
+// const app = new App({
+//     onError: (err, req, res) => {
+//         res.status(500).send({
+//             message: err.message,
+//         })
+//     },
+// })
 
-app
-    .use(cors({ origin: 'http://localhost:5000', allowedHeaders: ['Content-Type'] }))
-    .use(logger())
-    .use(bodyParser.json())
-    .get('/api/user/:email', async (req, res) => {
-        const query = `*[_type == 'user' && email == '${req.params.email}']{ _id, name, email, "userProfile":userProfile->{bio,timePreference} }`;
-        let usersReq = [];
-        try {
-            usersReq = await sanity.fetch(query);
-        } catch (e) {
-            usersReq = e;
-        }
-        res.status(200).send(JSON.stringify(usersReq));
-    })
-    .listen(process.env.PORT || 4000)
+// app
+//     .use(cors({ origin: 'http://localhost:5000', allowedHeaders: ['Content-Type'] }))
+//     .use(logger())
+//     .use(bodyParser.json())
+//     .get('/api/user/:email', async (req, res) => {
+//         const query = `*[_type == 'user' && email == '${req.params.email}']{ _id, name, email, "userProfile":userProfile->{bio,timePreference} }`;
+//         let usersReq = [];
+//         try {
+//             usersReq = await sanity.fetch(query);
+//         } catch (e) {
+//             usersReq = e;
+//         }
+//         res.status(200).send(JSON.stringify(usersReq));
+//     })
+
+// module.exports = app;
+// module.exports.handler = serverless(app);
--- serverLocal.js
+++
@@ -1,5 +0,0 @@
-'use strict';
-
-const app = require('./index');
-
-app.listen(4000, () => console.log('Local app listening on port 4000!'));
