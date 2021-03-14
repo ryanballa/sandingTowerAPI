a5763bea7edfafd9e0141fb1216960ee96212958
Ryan Balla <ryan@ryanballa.com>
2021-03-14 10:47:09 -0500

    Switch to tinyhttp

---
+++ index.js
@@ -0,0 +1,30 @@
+import { App } from '@tinyhttp/app'
+import { cors } from '@tinyhttp/cors'
+import { logger } from '@tinyhttp/logger'
+import bodyParser from 'body-parser'
+import sanity from './lib/sanity.js';
+
+const app = new App({
+    onError: (err, req, res) => {
+        res.status(500).send({
+            message: err.message,
+        })
+    },
+})
+
+app
+    .use(cors({ origin: 'http://localhost:8080', allowedHeaders: ['Content-Type'] }))
+    .use(logger())
+    .use(bodyParser.json())
+    .get('/api/user/:email', async (req, res) => {
+        const email = req.params.email;
+        const query = `*[_type == 'user' && email == '${email}']{ _id, name, email, "userProfile":userProfile->{bio,timePreference} }`;
+        let usersReq = [];
+        try {
+            usersReq = await sanity.fetch(query);
+        } catch (e) {
+            console.log(`Error: ${e}`);
+        }
+        res.status(200).json(usersReq);
+    })
+    .listen(process.env.PORT || 5000)
