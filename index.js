685d0e06d17ea28389ffde09d98f99496bdee77b
Ryan Balla <ryan@ryanballa.com>
2021-03-16 18:10:46 -0500

    Using express

--- index.js
+++ index.js
@@ -1,30 +1,38 @@
-import { App } from '@tinyhttp/app'
-import { cors } from '@tinyhttp/cors'
-import { logger } from '@tinyhttp/logger'
-import bodyParser from 'body-parser'
 import sanity from './lib/sanity.js';
+import cors from 'cors';
+import jwt from 'express-jwt';
+import jwksRsa from 'jwks-rsa';
+import express from 'express';
 
-const app = new App({
-    onError: (err, req, res) => {
-        res.status(500).send({
-            message: err.message,
-        })
-    },
-})
+const app2 = express();
 
-app
-    .use(cors({ origin: 'http://localhost:5000', allowedHeaders: ['Content-Type'] }))
-    .use(logger())
-    .use(bodyParser.json())
-    .get('/api/user/:email', async (req, res) => {
-        const email = req.params.email;
-        const query = `*[_type == 'user' && email == '${email}']{ _id, name, email, "userProfile":userProfile->{bio,timePreference} }`;
-        let usersReq = [];
-        try {
-            usersReq = await sanity.fetch(query);
-        } catch (e) {
-            console.log(`Error: ${e}`);
-        }
-        res.status(200).json(usersReq);
-    })
-    .listen(process.env.PORT || 5000)
+const checkJwt = jwt({
+    // Dynamically provide a signing key
+    // based on the kid in the header and 
+    // the signing keys provided by the JWKS endpoint.
+    secret: jwksRsa.expressJwtSecret({
+        cache: true,
+        rateLimit: true,
+        jwksRequestsPerMinute: 5,
+        jwksUri: `https://dev-1onipavl.us.auth0.com/.well-known/jwks.json`
+    }),
+
+    // Validate the audience and the issuer.
+    audience: '87L3BqxrCNlT2Cnqac8JQB37ybOb5l9K',
+    issuer: `https://dev-1onipavl.us.auth0.com/`,
+    algorithms: ['RS256']
+});
+
+app2.use(cors({ origin: 'http://localhost:5000', allowedHeaders: ['Content-Type', 'Authorization'] }));
+
+app2.get('/api/user/:email', checkJwt, async function (req, res) {
+    const email = req.params.email;
+    const query = `*[_type == 'user' && email == '${email}']{ _id, name, email, "profiles": *[ _type == "profile" && references(^._id)] }`;
+    let usersReq = [];
+    try {
+        usersReq = await sanity.fetch(query);
+    } catch (e) {
+        console.log(`Error: ${e}`);
+    }
+    res.status(200).json(usersReq);
+}).listen(process.env.PORT || 4000)
