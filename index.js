2875dadb3fce0bb9a7e1acaafc26b4e03268d284
Ryan Balla <ryan@ryanballa.com>
2021-03-18 21:54:34 -0500

    Adding endpoints for locomotives and user

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 import sanity from './lib/sanity.js';
+import bodyParser from 'body-parser';
 import cors from 'cors';
 import jwt from 'express-jwt';
 import jwksRsa from 'jwks-rsa';
 import express from 'express';
 
-const app2 = express();
+const app = express();
 
 const checkJwt = jwt({
     // Dynamically provide a signing key
@@ -23,9 +24,10 @@ const checkJwt = jwt({
     algorithms: ['RS256']
 });
 
-app2.use(cors({ origin: ['http://localhost:5000', 'https://locomotivehouse.com'], allowedHeaders: ['Content-Type', 'Authorization'] }));
+app.use(bodyParser.json());
+app.use(cors({ origin: ['http://localhost:5000', 'https://locomotivehouse.com'], allowedHeaders: ['Content-Type', 'Authorization'] }));
 
-app2.get('/api/user/:email', checkJwt, async function (req, res) {
+app.get('/api/user/:email', checkJwt, async function (req, res) {
     const email = req.params.email;
     const query = `*[_type == 'user' && email == '${email}']{ _id, name, email, "profiles": *[ _type == "profile" && references(^._id)] }`;
     let usersReq = [];
@@ -35,4 +37,50 @@ app2.get('/api/user/:email', checkJwt, async function (req, res) {
         console.log(`Error: ${e}`);
     }
     res.status(200).json(usersReq);
-}).listen(process.env.PORT || 4000)
+});
+
+app.post('/api/user', checkJwt, async function (req, res) {
+    const doc = req.body;
+    let userRes = null;
+    try {
+        userRes = await sanity.create(doc);
+    } catch (e) {
+        console.log(e);
+    }
+    res.status(200).json(userRes);
+});
+
+app.get('/api/clubs', async function (req, res) {
+    const query = `*[_type == 'club']{ _id, name }`;
+    let usersReq = [];
+    try {
+        usersReq = await sanity.fetch(query);
+    } catch (e) {
+        console.log(`Error: ${e}`);
+    }
+    res.status(200).json(usersReq);
+});
+
+app.get('/api/locomotives', checkJwt, async function (req, res) {
+    const query = `*[_type == 'locomotive']{ _id, address, engineType, road, roadNumber, "locomotiveOwner": owner->{_id, name} }`;
+    let locomotivesQuery = [];
+    try {
+        locomotivesQuery = await sanity.fetch(query);
+    } catch (e) {
+        locomotivesQuery = e;
+    }
+    res.status(200).json(locomotivesQuery);
+});
+
+app.post('/api/locomotives', checkJwt, async function (req, res) {
+    const doc = req.body;
+    let locomotveRes = null;
+    try {
+        locomotveRes = await sanity.create(doc);
+    } catch (e) {
+        console.log(e);
+    }
+    res.status(200).json(locomotveRes);
+});
+
+app.listen(process.env.PORT || 4000)
